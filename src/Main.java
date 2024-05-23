fc29b6a5d14ade61044349e7a7ed065c9bb57fee
YelyzavetaKo <yk66supa@studserv.uni-leipzig.de>
2024-05-23 17:28:15 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Manager manager = new Manager("src/Gedicht.txt");
+        Manager manager = new Manager("src/Gedicht1.txt");
         manager.ablauf();
     }
 }
