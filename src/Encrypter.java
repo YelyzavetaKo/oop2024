57187b3d85cc599621d6e39514381d043f02386b
tabse <69817301+sspinecho@users.noreply.github.com>
2024-05-25 18:22:45 +0200

    Update Encrypter.java

--- src/Encrypter.java
+++ src/Encrypter.java
@@ -1,21 +1,52 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Array;
+import java.math.BigInteger;
 import java.util.ArrayList;
-import java.io.BufferedReader;
 
 public class Encrypter {
 
-    private int[] pubKey;
-
-    this.Encrypter(int[] pubKey){
-        this.pubKey = pubKey;
+    int[] key;
+    public Encrypter(int p, int q) throws notAPrimeException {
+        Schluesselgenerator sg = new Schluesselgenerator(p,q);
+        this.key = sg.berechnePubKey();
     }
 
-    public ArrayList<Integer> konvertiereInNumveric(String text){
-        BufferedReader reader = new BufferedReader(text);
-        int r;
+    public int[] getKey(){
+        return key;
+    }
+
+    public ArrayList<Integer> konvertiereInNumeric(String text){
+        int numeric;
         ArrayList<Integer> numerics = new ArrayList<Integer>();
-        while ((r = reader.read()) != -1){
-            
-            numerics.add(Character.getNumericValue(r));
+        for (int i = 0; i < text.length(); i++) {
+            String s = String.valueOf(text.charAt(i));
+            try {
+                numeric = Byte.toUnsignedInt((s.getBytes("US-ASCII"))[0]);
+                numerics.add(numeric);
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
+            }
+            /*
+            //throws SonderzeichenException //einfuegen
+            if (numeric != -2) {
+                numerics.add(numeric);
+            } else {
+                throw new SonderzeichenException();
+            }
+             */
         }
+        System.out.println(numerics);
+        return numerics;
+    }
+
+    public ArrayList<Integer> encrypt(int[] pubKey, @NotNull ArrayList<Integer> numerics) {
+        ArrayList<Integer> encrypted = new ArrayList<Integer>();
+        for (int i = 0; i < numerics.size(); i++) {
+            BigInteger kPowE = new BigInteger(new BigInteger(Integer.toString(numerics.get(i))).pow(pubKey[0]).toByteArray());
+            encrypted.add(kPowE.intValue() % pubKey[1]);
+        }
+        return encrypted;
     }
 }
