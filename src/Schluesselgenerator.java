7671d2a2f379ea4d2311d047fabb209560fd49a5
tabse <69817301+sspinecho@users.noreply.github.com>
2024-05-25 18:22:18 +0200

    Update Schluesselgenerator.java

--- src/Schluesselgenerator.java
+++ src/Schluesselgenerator.java
@@ -1,53 +1,92 @@
-import org.jetbrains.annotations.NotNull;
+public class Schluesselgenerator {
 
-import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Array;
-import java.math.BigInteger;
-import java.util.ArrayList;
+    private int p;
+    private int q;
+    private int[] privKey = new int[2];
+    private int[] pubKey = new int[2];
 
-public class Encrypter {
-
-    int[] key;
-    public Encrypter(int p, int q) throws notAPrimeException {
-        Schluesselgenerator sg = new Schluesselgenerator(p,q);
-        this.key = sg.berechnePubKey();
-    }
-
-    public int[] getKey(){
-        return key;
-    }
-
-    public ArrayList<Integer> konvertiereInNumeric(String text){
-        int numeric;
-        ArrayList<Integer> numerics = new ArrayList<Integer>();
-        for (int i = 0; i < text.length(); i++) {
-            String s = String.valueOf(text.charAt(i));
-            try {
-                numeric = Byte.toUnsignedInt((s.getBytes("US-ASCII"))[0]);
-                numerics.add(numeric);
-            } catch (UnsupportedEncodingException e) {
-                throw new RuntimeException(e);
-            }
-            /*
-            //throws SonderzeichenException //einfuegen
-            if (numeric != -2) {
-                numerics.add(numeric);
-            } else {
-                throw new SonderzeichenException();
-            }
-             */
+    public Schluesselgenerator(int p, int q) throws notAPrimeException {
+        if (!pruefeObPrimzahl(p)) {
+            throw new notAPrimeException(p);
+        } else if (!pruefeObPrimzahl(q)) {
+            throw new notAPrimeException(q);
+        } else {
+            this.p = p;
+            this.q = q;
         }
-        System.out.println(numerics);
-        return numerics;
     }
 
-    public ArrayList<Integer> encrypt(int[] pubKey, @NotNull ArrayList<Integer> numerics) {
-        ArrayList<Integer> encrypted = new ArrayList<Integer>();
-        for (int i = 0; i < numerics.size(); i++) {
-            BigInteger kPowE = new BigInteger(new BigInteger(Integer.toString(numerics.get(i))).pow(pubKey[0]).toByteArray());
-            encrypted.add(kPowE.intValue() % pubKey[1]);
+    public int getP() {
+        return p;
+    }
+
+    public int getQ() {
+        return q;
+    }
+
+    public int getG() {
+        return g;
+    }
+
+    public boolean pruefeObPrimzahl(int mbPrimeNumber) {
+        boolean istPrimzahl = true;
+        for (int i = 2; i <= mbPrimeNumber / 2; ++i) {
+            if (mbPrimeNumber % i == 0) {
+                istPrimzahl = false;
+                break;
+            }
         }
-        return encrypted;
+        return istPrimzahl;
+    }
+
+    public int berechneGeneratorzahl() {
+        int g = getP() * getQ();
+        return g;
+    }
+
+    public int berechnePhi() {
+        int phi = (getP() - 1) * (getQ() - 1);
+        return phi;
+    }
+
+    public int getPhi() {
+        return phi;
+    }
+
+    public int findE() {
+        int e = 2;
+        while (berechneGGT(e, berechnePhi()) != 1) {
+            e++;
+        }
+        return e;
+    }
+
+    public int findD() {
+        int d = 0;
+        while ((d * findE()) % berechnePhi() != 1) {
+            d++;
+        }
+        return d;
+    }
+
+    public int berechneGGT(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return berechneGGT(b, a % b);
+        }
+    }
+
+    public int[] berechnePubKey(){
+        pubKey[0] = findE();
+        pubKey[1] = berechneGeneratorzahl();
+        return pubKey;
+    }
+
+    public int[] berechnePrivKEy(){
+        privKey[0] = findD();
+        privKey[1] = berechneGeneratorzahl();
+        return privKey;
     }
 }
 
