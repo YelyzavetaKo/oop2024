54c44c790fa6a7121318253bb8026542cbfcc845
tabse <69817301+sspinecho@users.noreply.github.com>
2024-05-29 19:42:54 +0200

    Update Encrypter.java
    comments added.
    calculation for modKPowE simplified so no type conversion is necessary.

--- src/Encrypter.java
+++ src/Encrypter.java
@@ -1,52 +1,67 @@
-import org.jetbrains.annotations.NotNull;
-
 import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Array;
-import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class Encrypter {
 
     int[] key;
-    public Encrypter(int p, int q) throws notAPrimeException {
+
+    /** Constructor
+     * Erstellt einen Schluesselgenerator, um mithilfe der Parameter zuer Verschluesselung einen oeffentlichen Schluessel zu erstellen.
+     * @param p Primzahl zur Schluesselgenerierung.
+     * @param q Primzahl zur Schluesselgenerierung.
+     * @throws NotAPrimeException Falls p oder q keine Primzahlen sind, kann kein Schluessel gemaess RSA-Verfahren generiert werden, deshalb wird in diesem Fall ein Fehler ausgeworfen.
+     */
+    public Encrypter(int p, int q) throws NotAPrimeException {
         Schluesselgenerator sg = new Schluesselgenerator(p,q);
         this.key = sg.berechnePubKey();
     }
 
+    /** Getter
+     *
+     * @return Gibt den oeffentlichen Schluessel zurueck, der der Verschluesselung dient.
+     */
     public int[] getKey(){
         return key;
     }
 
-    public ArrayList<Integer> konvertiereInNumeric(String text){
+    /**
+     * Zeilenumbrueche werden mit -1 codiert. Andere Zeichen werden nach Umwandlung in ByteValues mittels RSA codiert.
+     * @param plainText Zu codierender String.
+     * @see InputOutput hat diesen eingelesen.
+     * @return Gibt eine ArrayList mit allen in integer codierten Zeichen zurück.
+     */
+    public ArrayList<Integer> encrypt(String plainText){
         int numeric;
-        ArrayList<Integer> numerics = new ArrayList<Integer>();
-        for (int i = 0; i < text.length(); i++) {
-            String s = String.valueOf(text.charAt(i));
-            try {
-                numeric = Byte.toUnsignedInt((s.getBytes("US-ASCII"))[0]);
-                numerics.add(numeric);
-            } catch (UnsupportedEncodingException e) {
-                throw new RuntimeException(e);
-            }
-            /*
-            //throws SonderzeichenException //einfuegen
-            if (numeric != -2) {
-                numerics.add(numeric);
-            } else {
-                throw new SonderzeichenException();
-            }
-             */
-        }
-        System.out.println(numerics);
-        return numerics;
-    }
-
-    public ArrayList<Integer> encrypt(int[] pubKey, @NotNull ArrayList<Integer> numerics) {
+        int[] pubKey = getKey();
         ArrayList<Integer> encrypted = new ArrayList<Integer>();
-        for (int i = 0; i < numerics.size(); i++) {
-            BigInteger kPowE = new BigInteger(new BigInteger(Integer.toString(numerics.get(i))).pow(pubKey[0]).toByteArray());
-            encrypted.add(kPowE.intValue() % pubKey[1]);
+        for (int i = 0; i < plainText.length(); i++){
+            String s = String.valueOf(plainText.charAt(i));
+            if (s.equals("\n")){
+                encrypted.add(-1);
+            } else {
+                try {
+                    numeric = Byte.toUnsignedInt((s.getBytes("US-ASCII"))[0]);
+                    encrypted.add(calculateModuloOfKPowE(numeric));
+                } catch (UnsupportedEncodingException e) {
+                    throw new RuntimeException("Zeichen <" + s + "> kann nicht codiert werden.");
+                }
+            }
         }
         return encrypted;
     }
+
+    /**
+     * Berechnet k hoch e fuer das RSA-Verfahren. Da die Zahlen recht groß werden koennen, wird mit dieser schrittweisen Berechnung eine unnoetige Typkonvertierung vermieden.
+      * @param k ist der zu verschluesselnde int-Wert.
+     * @return Gibt das Ergebnis der Verschluesselung zurueck.
+     */
+    public int calculateModuloOfKPowE(int k){
+        int mod = 1;
+        for (int i = 0; i < getKey()[0]; i++){
+            mod = mod * k % getKey()[1];
+            System.out.println(mod);
+        }
+        return mod;
+    }
+
 }
